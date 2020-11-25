800e21b9ddb8e4d499dfe61a6784fa3ebe962503
Pritam Sukumar Kininge <kininge@users.noreply.github.com>
2020-11-25 15:50:27 +0530

    Add files via upload
    User tracking extention

---
+++ LetX/event.js
@@ -0,0 +1,13 @@
+chrome.contextMenus.create
+(
+    {
+        "id": "getXpth",
+        "title": "Get Xpath",
+        "contexts": ["all"]
+    }
+);
+
+chrome.contextMenus.onClicked.addListener(function(info, tab)
+{
+    alert(info.toString());
+});
