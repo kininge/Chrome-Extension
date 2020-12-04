945a7d1ed05b31970ee7f274a809c7e6322f47d4
Pritam Kininge <kininge007@gmail.com>
2020-12-04 12:29:47 +0530

    Working Angular for Chrome-Extension

--- Hyper-Automation/event.js
+++ Hyper-Automation/event.js
@@ -5,7 +5,6 @@ chrome.runtime.onMessageExternal.addListener( function(request, sender, sendResp
     {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) 
         {
-            console.log(tabs);
             chrome.tabs.sendMessage(tabs[0].id, {request: "getDOM"}, function(response) 
             {
                 var DOM= response.message;
